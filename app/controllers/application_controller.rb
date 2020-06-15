b94515dcd7f042c9135fe4742c5bf1519f54baca
KeiMaeLabo <65949665+KeiMaeLabo@users.noreply.github.com>
2020-06-15 18:34:52 +0900

    registration finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  protected
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age, :sex, :disease, :history, :status, :sns, :word])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :age, :sex, :disease, :history, :status, :sns, :word])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
+  before_action :move_to_index, except: :index
+
   def index
+    @users = User.all.order("created_at DESC")
   end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :age, :sex, :email, :disease, :history, :status, :sns, :word)
+  end
+
 end
