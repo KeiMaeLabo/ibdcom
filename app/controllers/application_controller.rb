fb2c47ef74ae9868b7bfca7150aadc82d7e2e193
KeiMaeLabo <65949665+KeiMaeLabo@users.noreply.github.com>
2020-06-16 18:28:20 +0900

    add privacy 2 finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age, :sex, :disease, :history, :status, :sns, :word, :image])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :age, :sex, :disease, :history, :status, :sns, :word, :image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age, :privacy, :sex, :disease, :history, :status, :sns, :word, :image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :age, :privacy, :sex, :disease, :history, :status, :sns, :word, :image])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :name, presence: true, uniqueness: true, length: { maximum: 10}
   validates :word, length: { maximum: 16}
-  validates :sns, inclusion: { in: %w(twitter instagram facebook youtube) }
+  # validates :sns, inclusion: { in: %w(twitter instagram facebook youtube) }
   devise :validatable, password_length: 6..50
 
   def update_without_current_password(params, *options)
