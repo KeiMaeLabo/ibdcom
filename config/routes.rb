f40c0d9a32fced6baed4e9fe15bef8095c7defa7
KeiMaeLabo <65949665+KeiMaeLabo@users.noreply.github.com>
2020-06-17 12:12:53 +0900

    g controller posts comments, routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: "users#index"
   devise_for :users,　controllers: { registrations: 'users/registrations' }
   resources :users, only: [:index, :edit, :update]
+  resources :posts, only: :index
 
 end
