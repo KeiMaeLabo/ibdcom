88a5fc9400cff6630407963481e66511b681741b
KeiMaeLabo <65949665+KeiMaeLabo@users.noreply.github.com>
2020-06-23 20:40:10 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,17 +11,17 @@ class User < ApplicationRecord
   # validates :sns, inclusion: { in: %w(twitter instagram facebook youtube) }
   devise :validatable, password_length: 6..50
 
-  def update_without_current_password(params, *options)
-    params.delete(:current_password)
+  # def update_without_current_password(params, *options)
+  #   params.delete(:current_password)
 
-    if params[:password].blank? && params[:password_confirmation].blank?
-      params.delete(:password)
-      params.delete(:password_confirmation)
-    end
+  #   if params[:password].blank? && params[:password_confirmation].blank?
+  #     params.delete(:password)
+  #     params.delete(:password_confirmation)
+  #   end
 
-    result = update_attributes(params, *options)
-    clean_up_passwords
-    result
-  end
+  #   result = update_attributes(params, *options)
+  #   clean_up_passwords
+  #   result
+  # end
 
 end
