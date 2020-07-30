02b943a8299a8e8ef7f1f6c8212267210da78967
KeiMaeLabo <65949665+KeiMaeLabo@users.noreply.github.com>
2020-07-30 21:59:19 +0900

    Update comments_controller.rb

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -4,11 +4,6 @@ class Api::CommentsController < ApplicationController
     # ajaxで送られてくる最後のコメントのid番号を変数に代入
     last_comment_id = params[:id]
     # 取得した質問でのコメント達から、idがlast_comment_idよりも新しい(大きい)メッセージ達のみを取得
-    if Rails.env.production?
-      @comments = post.comments.includes(:user).where(id > last_comment_id)
-    elsif Rails.env.development?
-      @comments = post.comments.includes(:user).where("id > ?", last_comment_id)
-    end
-
+    @comments = post.comments.includes(:user).where("id > ?", last_comment_id)
   end
 end
